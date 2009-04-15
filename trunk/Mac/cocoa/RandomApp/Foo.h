5bed1559ad200b3ed303ed0d9a912017411b9a06
hit456 <hit456@c41f4b8a-b930-11dd-8af8-77a15857c330>
2009-04-15 16:53:27 +0000

    new

---
+++ trunk/Mac/cocoa/RandomApp/Foo.h
@@ -0,0 +1,16 @@
+//
+//  Foo.h
+//  RandomApp
+//
+//  Created by luke on 4/16/09.
+//  Copyright 2009 luke. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface Foo : NSObject {
+
+}
+
+@end
---
+++ trunk/Mac/cocoa/RandomApp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  RandomApp
+//
+//  Created by luke on 4/16/09.
+//  Copyright luke 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
