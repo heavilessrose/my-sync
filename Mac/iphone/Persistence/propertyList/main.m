7ceecbfc5d085718c417f811e855f002e6aee325
hit456 <hit456@c41f4b8a-b930-11dd-8af8-77a15857c330>
2009-04-30 08:53:04 +0000

    数据持久化之一 ： 属性列表

---
+++ Mac/iphone/Persistence/propertyList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  propertyList
+//
+//  Created by wang luke on 4/30/09.
+//  Copyright luke 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
