86430895652f8fa4af5cfe5978a298688e738c19
hit456 <hit456@c41f4b8a-b930-11dd-8af8-77a15857c330>
2009-05-04 13:35:14 +0000

    settings and user defaults

---
+++ Mac/iphone/settings&userDefaults/AppSettings/Classes/MainView.m
@@ -0,0 +1,32 @@
+//
+//  MainView.m
+//  AppSettings
+//
+//  Created by luke on 5/2/09.
+//  Copyright luke 2009. All rights reserved.
+//
+
+#import "MainView.h"
+
+@implementation MainView
+
+
+- (id)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
