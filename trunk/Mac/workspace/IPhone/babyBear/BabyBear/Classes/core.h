1fdf85bb127b2c7093f4c1a29955267344a34e7a
hit456 <hit456@c41f4b8a-b930-11dd-8af8-77a15857c330>
2010-08-03 10:43:13 +0000

    init babyBear

---
+++ trunk/Mac/workspace/IPhone/babyBear/BabyBear/Classes/core.h
@@ -0,0 +1,26 @@
+/*
+ *  core.h
+ *  BabyBear
+ *
+ *  Created by luke on 8/3/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef _BBEAR_CORE_H_
+#define _BBEAR_CORE_H_
+
+typedef struct BBearCore BBearCore;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+    
+    
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _BBEAR_CORE_H_ */
