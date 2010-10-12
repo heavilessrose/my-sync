4945f87c6f1e9ee89d74889b0966cc887e042c23
hit456@gmail.com <hit456@gmail.com@c41f4b8a-b930-11dd-8af8-77a15857c330>
2010-10-12 01:55:03 +0000

    iphone ipad universal project template init commit

---
+++ trunk/Mac/workspace/iUniversTemplate/iPad/AppDelegate_iPad.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate_iPad.h
+//  iUniversTemplate
+//
+//  Created by luke on 10-10-12.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPad : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
