a06e085822c4397c9a560fd71c6a1181548e8c13
hit456 <hit456@c41f4b8a-b930-11dd-8af8-77a15857c330>
2011-06-12 03:24:11 +0000

    '

---
+++ work/SLVod/SLVod/SLVod/Defines.h
@@ -0,0 +1,47 @@
+//
+//  Defines.h
+//  siluVod
+//
+//  Created by luke on 11-6-1.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "siluVodAppDelegate.h"
+#import "LKViewController.h"
+
+#define globalApp ((siluVodAppDelegate *)[UIApplication sharedApplication].delegate)
+
+#define RGBCOLOR(r,g,b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1]
+#define RGBACOLOR(r,g,b,a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)]
+
+
+
+// LOG
+#define LK_DEBUG
+#define LK_MEM_DEBUG
+
+#ifdef LK_DEBUG 
+#define NSLog(...) NSLog(__VA_ARGS__)
+#else
+#define NSLog(...) 
+#endif
+
+#ifdef LK_DEBUG
+#define DLog(...) NSLog(@"%s %@", __PRETTY_FUNCTION__, [NSString stringWithFormat:__VA_ARGS__])
+#define ALog(...) [[NSAssertionHandler currentHandler] handleFailureInFunction:[NSString stringWithCString:__PRETTY_FUNCTION__ encoding:NSUTF8StringEncoding] file:[NSString stringWithCString:__FILE__ encoding:NSUTF8StringEncoding] lineNumber:__LINE__ description:__VA_ARGS__]
+#else
+#define DLog(...) do { } while (0)
+#ifndef NS_BLOCK_ASSERTIONS
+#define NS_BLOCK_ASSERTIONS
+#endif
+#define ALog(...) NSLog(@"%s %@", __PRETTY_FUNCTION__, [NSString stringWithFormat:__VA_ARGS__])
+#endif
+
+#ifdef LK_MEM_DEBUG
+#define MLog(...) NSLog(@"%s %@", __PRETTY_FUNCTION__, [NSString stringWithFormat:__VA_ARGS__])
+#else
+#define MLog(...) do {} while (0)
+#endif
+
+#define ZAssert(condition, ...) do { if (!(condition)) { ALog(__VA_ARGS__); }} while(0)
+
