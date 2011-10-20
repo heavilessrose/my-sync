60f31bf0f885fe1f4908946b4f786f391ec98d03
hit456 <hit456@gmail.com>
2011-10-20 12:00:27 +0000

    init

---
+++ Mac/workspace/C/libLKIM/LK_sock.h
@@ -0,0 +1,10 @@
+#ifndef __LK_SOCK_H__
+#define __LK_SOCK_H__
+
+/*
+socket abstraction
+*/
+#include "LK_types.h"
+
+#endif /* __LK_SOCK_H__ */
+
---
+++ Mac/workspace/C/libLKIM/LK_types.h
@@ -0,0 +1,9 @@
+#ifndef __LK_SOCK_H__
+#define __LK_SOCK_H__
+
+/*
+Declaration of basic types
+*/
+
+#endif /* __LK_SOCK_H__ */
+
